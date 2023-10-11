6dfef02cf523b933c4292beb2e5f7d81b2ad951a
CrystalSnake <22963880+CrystalSnake@users.noreply.github.com>
2023-10-11 21:35:34 +0400

    Add docstrings to Utilities

--- src/Utilities.jsx
+++ src/Utilities.jsx
@@ -1,3 +1,9 @@
+/**
+ * Calculates the sum of the quantities of items in a shopping cart.
+ *
+ * @param {Array} cart - An array of objects representing items in a shopping cart. Each object has a `quantity` property indicating the quantity of the item.
+ * @returns {number} - The sum of the quantities of all items in the shopping cart.
+ */
 const sumOfProducts = (cart) => {
   const sum = cart.reduce(function (currentSum, currentItem) {
     const curNumber = Number(currentItem.quantity);
@@ -6,6 +12,22 @@ const sumOfProducts = (cart) => {
   return sum;
 };
 
+/**
+ * Calculates the subtotal of products in a shopping cart.
+ *
+ * @param {Array} cart - An array of objects representing the products in the shopping cart. Each object should have properties `quantity` and `price`.
+ * @returns {number} - The total cost of all products in the shopping cart.
+ *
+ * @example
+ * const cart = [
+ *   { name: 'Product 1', quantity: 2, price: 10 },
+ *   { name: 'Product 2', quantity: 1, price: 5 },
+ *   { name: 'Product 3', quantity: 3, price: 8 }
+ * ];
+ *
+ * const subtotal = subtotalOfProducts(cart);
+ * console.log(subtotal); // Output: 49
+ */
 const subtotalOfProducts = (cart) => {
   const subtotal = cart.reduce(function (currentSub, currentItem) {
     const curPrice = Number(currentItem.quantity) * Number(currentItem.price);
